2f6be106a00d94bee46dd833bbcdaac56ac37e8a
Erik-Chan <46538091+Erik-Chan@users.noreply.github.com>
2020-08-15 06:07:08 -0600

    Updated code and notes
    Started working on the LP problem. Updated some notes on the LP problem notation.

--- Ceno.py
+++ Ceno.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sdepy
+import itertools
 #Plot Parameters
 ###########################################
 plt.rcParams['figure.figsize'] = (20, 6)
@@ -15,7 +16,7 @@ def my_ou(t, x, theta=1., k=1., sigma=1.):
 
 class Edge():
 
-    # Initialize the matrix
+    # Initialize the edge set
     def __init__(self, size = 0):
         self.edge = []
         self.edge = [[i,i] for i in range(0,size)]
@@ -42,7 +43,7 @@ class Edge():
         return self.edge
 
 class Vertex():
-    # Initialize the matrix
+    # Initialize the vertex set
     def __init__(self, vertices = []):
         self.vertex = []
         self.vertex = [x for x in vertices]
@@ -59,8 +60,35 @@ class Vertex():
             return
         self.vertex.remove(v1)
 
+    def __len__(self):
+        return len(self.vertex)
+
     def get_vertex(self):
         return self.vertex
+
+class MSEopt():
+    def __init__(self, beta = 0.3, sizeS = 5):
+        self.x_sum = 0
+        self.beta = beta
+        self.sizeS = sizeS
+    def eval_obj_fun(self, x):
+        self.x_sum = np.sum(x)
+        return self.x_sum
+        pass
+    def set_beta(self, beta):
+        self.beta = beta
+    def set_lambda_s(self, lmbda):
+        self.lmbda = lmbda
+    def set_M(self, lambda_s, vertex):
+        vertex_perms =  itertools.permutations(S,2)
+        m = max([sum(v) for v in list(vertex_perms)])
+        #Still need to maximize for all t
+        pass
+
+
+
+
+
 
 
 toggle = 0
