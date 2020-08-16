c530c0f19c1dc36958990127053a9954b0937a01
Erik-Chan <46538091+Erik-Chan@users.noreply.github.com>
2020-08-16 15:12:40 -0600

    Update Ceno.py
    Some miscellaneous tinkering

--- Ceno.py
+++ Ceno.py
@@ -3,12 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sdepy
 import itertools
+
+#tools needed for TSP
+from itertools import product
+from sys import stdout as out
+from mip import Model, xsum, minimize, BINARY
+
 #Plot Parameters
 ###########################################
 plt.rcParams['figure.figsize'] = (20, 6)
 plt.rcParams['lines.linewidth'] = 1.
 ###########################################
-
+np.random.seed(1)
 
 @sdepy.integrate
 def my_ou(t, x, theta=1., k=1., sigma=1.):
@@ -27,17 +33,21 @@ class Edge():
         if [v1, v2] in self.edge:
             print("Edge from %d to %d already exists" % (v1, v2))
             return
-        self.adjMatrix.append([v1,v2])
+        self.edge.append([v1,v2])
 
     # Remove edges
     def remove_edge(self, v1, v2):
         if [v1,v2] not in self.edge:
             print("No edge between %d and %d" % (v1, v2))
             return
-        self.adjMatrix.remove([v1,v2])
+        self.edge.remove([v1,v2])
 
     def __len__(self):
         return self.size
+
+    def __add__(self, other):
+        add_edge(other[0],other[1])
+        return self
 
     def get_edge(self):
         return self.edge
@@ -66,6 +76,10 @@ class Vertex():
     def get_vertex(self):
         return self.vertex
 
+    def __add__(self, other):
+        self.add_vertex(other)
+        return self
+
 class MSEopt():
     def __init__(self, beta = 0.3, sizeS = 5):
         self.x_sum = 0
@@ -75,6 +89,8 @@ class MSEopt():
         self.x_sum = np.sum(x)
         return self.x_sum
         pass
+    def set_T(self,T):
+        self.T = T
     def set_beta(self, beta):
         self.beta = beta
     def set_lambda_s(self, lmbda):
@@ -84,12 +100,9 @@ class MSEopt():
         m = max([sum(v) for v in list(vertex_perms)])
         #Still need to maximize for all t
         pass
-
-
-
-
-
-
+    def get_psi_u(self):
+        self.psi_u = np.floor(self.beta*self.T)
+        return self.psi_u
 
 toggle = 0
 if toggle:
@@ -97,3 +110,33 @@ if toggle:
     x = my_ou(x0=1, paths=3, steps=100)(timeline)
     plt.plot(x)
     plt.show()
+
+
+MSE = 1
+
+if MSE:
+    verts = Vertex(vertices = ['Calgary', 'Boston', 'Cushing'])
+
+    edges = Edge(size = 3)
+    edges.add_edge(1,2)
+    edges.add_edge(1,3)
+    edges.add_edge(2,3)
+    #costs = {e:np.random.uniform(0,1) for e in edges.get_edge()}
+    costs = {tuple(e): np.random.uniform(0,1) for e in edges.get_edge()}
+
+    model = Model()
+    T = 1
+    t = np.linspace(0,T,10)
+    # These are the psi^t
+    x = [model.add_var(var_type=BINARY) for _t in t]
+    # These are the gamma_s^t
+    xx = [[model.add_var(var_type=BINARY) for _t in t] for s in verts.get_vertex()]
+
+    #These are the alpha_s
+    alpha_s = [model.add_var() for s in verts.get_vertex()]
+
+
+    #model += alpha_s[1] >= 500
+    model.objective = minimize(sum(alpha_s[i] for i in set(range(len(verts)))))
+    model.optimize()
+    print('the solution at the minimum is :', alpha_s[0].x, alpha_s[1].x, alpha_s[2].x)
