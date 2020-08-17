8aefd49bb1057528654c52030dc818bc148501a5
Erik-Chan <46538091+Erik-Chan@users.noreply.github.com>
2020-08-17 13:10:06 -0600

    Update Ceno.py

--- Ceno.py
+++ Ceno.py
@@ -92,11 +92,11 @@ class MSEopt:
         self.beta = beta
         self.sizeS = sizeS
         self.T = T
+        self.obj_v_sum = 0
 
     def eval_obj_fun(self, obj_v):
         self.obj_v_sum = np.sum(obj_v)
         return self.obj_v__sum
-        pass
 
     def set_T(self, T):
         assert isinstance(T, float)
@@ -122,10 +122,10 @@ class MSEopt:
 toggle = 0
 plot_toggle = 0
 if toggle:
-    timeline = np.linspace(0., 1., 10)
+    timeline = np.linspace(0., 1., 100)
     # x = my_ou(x0=1, paths=3, steps=100)(timeline)
-    x = my_ou(x0=30, k=2, theta=30, sigma=5, paths=3, steps=10)(timeline)
-    print([row[0] for row in x])
+    x = my_ou(x0=30, k=2, theta=30, sigma=5, paths=3, steps=100)(timeline)
+    #print([row[0] for row in x])
     if plot_toggle:
         plt.plot(x)
         plt.show()
@@ -135,29 +135,32 @@ MSE = 1
 if MSE:
     # Time
     T = 1
-    t = np.linspace(0, T, 10)
+    t = np.linspace(0, T, 1000)
     # The set \mathcal{S}
-    verts = Vertex(vertices=['Calgary', 'Boston', 'Cushing'])
+    verts = Vertex(vertices=['Calgary', 'Hardesty', 'Cushing'])
 
     edges = Edge(size=3)
     edges.add_edge(1, 2)
     edges.add_edge(1, 3)
     edges.add_edge(2, 3)
     # costs = {e:np.random.uniform(0,1) for e in edges.get_edge()}
-    costs = {tuple(e): np.random.uniform(0, 1) for e in edges.get_edge()}
+    transport_costs = {tuple(e): np.random.uniform(0, 1) for e in edges.get_edge()}
 
     # Note that this should eventually look like [[something for _t in t] for s in verts.get_vertex()]
     # This current version just populates placeholder values.
 
     # This is the \lambda_{s}^{t}
-    localPrices = my_ou(x0=30, k=2, theta=30, sigma=5, paths=3, steps=10)(t)
+    localPrices = my_ou(x0=30, k=2, theta=30, sigma=5, paths=3, steps=1000)(t)
 
     model = Model()
 
-    # z = m.add_var(name='zCost', var_type=INTEGER, lb=-10, ub=10)
+    ###################################################################################################################
+    # Variables
+    ###################################################################################################################
 
     # These are the psi^t
     psi_t = [model.add_var(var_type=BINARY) for _t in t]
+    
     # These are the gamma_s^t
     gamma_st = [[model.add_var(var_type=BINARY) for _t in t] for s in verts.get_vertex()]
 
@@ -173,78 +176,52 @@ if MSE:
     # These are \bar{w}_{s}^{t}.
     til_W_st = [[model.add_var() for _t in t] for s in verts.get_vertex()]
 
-    # model += alpha_s[1] >= 500
-    model.objective = minimize(xsum(alpha_s[i] for i in set(range(len(verts)))))
-    model.optimize()
-    print('The solution at the minimum is :', alpha_s[0].x, alpha_s[1].x, alpha_s[2].x)
+    ###################################################################################################################
+    # Constraints
+    ###################################################################################################################
 
-TSP = 0
-if TSP:
-    # names of places to visit
-    places = ['Antwerp', 'Bruges', 'C-Mine', 'Dinant', 'Ghent',
-              'Grand-Place de Bruxelles', 'Hasselt', 'Leuven',
-              'Mechelen', 'Mons', 'Montagne de Bueren', 'Namur',
-              'Remouchamps', 'Waterloo']
+    #Create the constraints \lambda_{s}^{t} = \eta^{t} + \rho_{s} + \alpha_{s} + \bar{w}_{s}^{t}
+    for s in range(len(verts)):
+        rho = rho_s[s]
+        alpha = alpha_s[s]
+        til_W_t = til_W_st[s]
+        localPrice_t = [row[s] for row in localPrices]
+        for _t in range(len(t)):
+            til_W = til_W_t[_t]
+            eta = eta_t[_t]
+            localPrice = localPrice_t[_t]
+            model += eta + rho + alpha + til_W == localPrice
 
-    # distances in an upper triangular matrix
-    dists = [[83, 81, 113, 52, 42, 73, 44, 23, 91, 105, 90, 124, 57],
-             [161, 160, 39, 89, 151, 110, 90, 99, 177, 143, 193, 100],
-             [90, 125, 82, 13, 57, 71, 123, 38, 72, 59, 82],
-             [123, 77, 81, 71, 91, 72, 64, 24, 62, 63],
-             [51, 114, 72, 54, 69, 139, 105, 155, 62],
-             [70, 25, 22, 52, 90, 56, 105, 16],
-             [45, 61, 111, 36, 61, 57, 70],
-             [23, 71, 67, 48, 85, 29],
-             [74, 89, 69, 107, 36],
-             [117, 65, 125, 43],
-             [54, 22, 84],
-             [60, 44],
-             [97],
-             []]
+    #Nonnegativity for alpha and rho
+    for s in range(len(verts)):
+        model += alpha_s[s] >= 0
+        model += rho_s[s] >= 0
 
-    # number of nodes and list of vertices
-    n, V = len(dists), set(range(len(dists)))
+    #Constraint (20e)
+    beta = 0.15
+    model += xsum(psi_t) <= np.floor(beta*T)
 
-    # distances matrix
-    c = [[0 if i == j else dists[i][j - i - 1] if j > i
-    else dists[j][i - j - 1] for j in V] for i in V]
+    #Constraint (20f)
+    #Placeholder for M
+    M = 100
+    for s in range(len(verts)):
+        til_W_t = til_W_st[s]
+        gamma_t = gamma_st[s]
+        alpha = alpha_s[s]
+        for _t in range(len(t)):
+            model += til_W_t[_t] <= -2*alpha + (1-gamma_t[_t])*M
 
-    model = Model()
+    #Constraint (20g)
+    #for _t in range(len(t)):
 
-    # binary variables indicating if arc (i,j) is used on the route or not
-    x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
+    #Transposing gamma_st
+    gamma_ts = [list(i) for i in zip(*gamma_st)]
+    for _t in range(len(t)):
+        model += xsum(gamma_ts[_t]) >= psi_t[_t]
 
-    # continuous variable to prevent subtours: each city will have a
-    # different sequential id in the planned route except the first one
-    y = [model.add_var() for i in V]
-
-    # objective function: minimize the distance
-    model.objective = minimize(xsum(c[i][j] * x[i][j] for i in V for j in V))
-
-    # constraint : leave each city only once
-    for i in V:
-        model += xsum(x[i][j] for j in V - {i}) == 1
-
-    # constraint : enter each city only once
-    for i in V:
-        model += xsum(x[j][i] for j in V - {i}) == 1
-
-    # subtour elimination
-    for (i, j) in product(V - {0}, V - {0}):
-        if i != j:
-            model += y[i] - (n + 1) * x[i][j] >= y[j] - n
-
-    # optimizing
-    model.optimize()
-
-    # checking if a solution was found
-    if model.num_solutions:
-        out.write('route with total distance %g found: %s'
-                  % (model.objective_value, places[0]))
-        nc = 0
-        while True:
-            nc = [i for i in V if x[nc][i].x >= 0.99][0]
-            out.write(' -> %s' % places[nc])
-            if nc == 0:
-                break
-        out.write('\n')
+    model.objective = minimize(xsum(alpha_s[i] for i in range(len(verts))))
+    toggle_optimize = 1
+    if toggle_optimize:
+        model.optimize()
+        print('The solution at the minimum is :', alpha_s[0].x, alpha_s[1].x, alpha_s[2].x)
+        print('The solution at the minimum is :', rho_s[0].x, rho_s[1].x, rho_s[2].x)
